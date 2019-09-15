3c131dfcb887729c7e651fd09cf3bce8a694df81
VampKnightDude <43006834+VampKnightDude@users.noreply.github.com>
2019-09-15 18:13:59 +0530

    Update Pacman.java

--- Pacman.java
+++ Pacman.java
@@ -1,1 +1,26 @@
-HELLLOOO
+import java.util.*;
+
+class TheProgram
+{
+	public static main void{String args[])
+	{
+		int exit=0, c;
+		Scanner scan=new Scanner(Sytem.in);
+		System.out.println("This is The Program.");
+		
+		do
+		{
+			System.out.pritnln("Make choice: \n 1) Exit.");
+			c=scan.nextInt();
+			switch(c)
+			{
+				case 1:
+					exit=1;
+					break;
+				default:
+					System.out.println("\a\nERROR: Invalid Menu Input);
+					break;
+			}
+		}while(exit==0);
+	}
+}
